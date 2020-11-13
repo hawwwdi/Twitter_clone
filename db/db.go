c5bd392ca1c14189ea82947ef8b2df45b0ff1a5d
hadi <hawwwdi@gmail.com>
2020-11-13 21:46:19 +0330

    add constants

--- db/db.go
+++ db/db.go
@@ -4,35 +4,44 @@ import "github.com/go-redis/redis"
 
 var rdb *redis.Client
 
+const (
+	lastIdC   = "last_id"
+	usernameC = "username"
+	passwordC = "password"
+	usersMapC = "users"
+)
+
 func init() {
+	//todo get port from environment variables
 	rdb = redis.NewClient(&redis.Options{
 		Addr: "localhost:6037",
 	})
 	_, err := rdb.Ping().Result()
 	checkErr(err)
-	err = rdb.Set("last_id", "0", 0).Err()
+	err = rdb.Set(lastIdC, "0", 0).Err()
 	checkErr(err)
 }
 
 func AddUser(usr user) error {
 	var err error
 	_, username, password := usr.info()
-	last_id, err := rdb.Get("last_id").Result()
-	id := "user:" + last_id
-	err = rdb.HSet(id, "username", username).Err()
+	lastID, err := rdb.Get(lastIdC).Result()
+	checkErr(err)
+	id := "user:" + lastID
+	err = rdb.HSet(id, usernameC, username).Err()
 	if err != nil {
 		return err
 	}
-	err = rdb.HSet(id, "password", password).Err()
+	err = rdb.HSet(id, passwordC, password).Err()
 	if err != nil {
 		return err
 	}
 	usr.setId(id)
-	err = rdb.HSet("users", username, last_id).Err()
+	err = rdb.HSet(usersMapC, username, lastID).Err()
 	if err != nil {
 		return err
 	}
-	err = rdb.Incr("last_id").Err()
+	err = rdb.Incr(lastIdC).Err()
 	checkErr(err)
 	return nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/hawwwdi/Twitter_clone
 
 go 1.15
 
-require github.com/go-redis/redis/v8 v8.3.3 // indirect
+require (
+	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/go-redis/redis/v8 v8.3.3 // indirect
+)
