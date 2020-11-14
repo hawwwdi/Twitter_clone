f89441e851a4679c64f65dca4e1790afa3e28103
hadi <hawwwdi@gmail.com>
2020-11-14 19:13:22 +0330

    update main package

--- main.go
+++ main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"flag"
+	"log"
 	"net/http"
 
 	"github.com/hawwwdi/Twitter_clone/api"
-	_ "github.com/hawwwdi/Twitter_clone/db"
+	"github.com/hawwwdi/Twitter_clone/db"
 )
 
 func main() {
-	mux := api.NewRouter()
-	panic(http.ListenAndServe(":8080", mux))
+	dbPort := flag.String("dbp", "6037", "redis server port")
+	serverPort := flag.String("sp", "8080", "server port")
+	flag.Parse()
+	db := db.NewDB("localhost:" + *dbPort)
+	api.InitHub(db)
+	hub, err := api.GetHub()
+	if err != nil {
+		panic(err)
+	}
+	log.Fatalln(http.ListenAndServe("localhost:"+*serverPort, hub.Router))
 }
