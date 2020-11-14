08b8cd6fce1fc8a8be4f3f0f915cdbee7458a4fd
hadi <hawwwdi@gmail.com>
2020-11-14 14:24:14 +0330

    refactore db package

--- db/db.go
+++ db/db.go
@@ -10,8 +10,6 @@ import (
 	gouuid "github.com/satori/go.uuid"
 )
 
-var rdb *redis.Client
-
 const (
 	lastIdC   = "last_id"
 	lastPostC = "last_post"
@@ -20,10 +18,13 @@ const (
 	usersMapC = "users"
 )
 
-func init() {
-	//todo get port from environment variables
-	rdb = redis.NewClient(&redis.Options{
-		Addr: "localhost:6037",
+type DB struct {
+	rdb *redis.Client
+}
+
+func NewDB(addr string) *DB {
+	rdb := redis.NewClient(&redis.Options{
+		Addr: addr,
 	})
 	_, err := rdb.Ping().Result()
 	checkErr(err)
@@ -31,12 +32,43 @@ func init() {
 	checkErr(err)
 	err = rdb.Set(lastPostC, "0", 0).Err()
 	checkErr(err)
+	return &DB{
+		rdb: rdb,
+	}
 }
 
-func RegisterUser(usr user) (string, error) {
+func (d *DB) RegisterUser(usr user) (string, error) {
+	return registerUser(d.rdb, usr)
+}
+
+func (d *DB) LogIn(username, password string) (string, error) {
+	return logIn(d.rdb, username, password)
+}
+
+func (d *DB) LogOut(session string) error {
+	return logOut(d.rdb, session)
+}
+
+func (d *DB) Follow(follower, followed string) error {
+	return follow(d.rdb, follower, followed)
+}
+
+func (d *DB) Post(body, owner string) error {
+	return post(d.rdb, body, owner)
+}
+
+func (d *DB) ShowUserPosts(id string, start, count int) (map[string][]string, error) {
+	return showUserPosts(d.rdb, id, int64(start), int64(count))
+}
+
+func (d *DB) GetSessionUserID(session string) (string, error) {
+	return getSessionUserID(d.rdb, session)
+}
+
+func registerUser(rdb *redis.Client, usr user) (string, error) {
 	var err error
 	_, username, password := usr.Info()
-	exists := checkUsername(username) != ""
+	exists := checkUsername(rdb, username) != ""
 	if exists {
 		return "", fmt.Errorf("user %v already exists", username)
 	}
@@ -61,17 +93,17 @@ func RegisterUser(usr user) (string, error) {
 	return lastID, nil
 }
 
-func LogIn(username, password string) (string, error) {
+func logIn(rdb *redis.Client, username, password string) (string, error) {
 	var err error
-	id := checkUsername(username)
+	id := checkUsername(rdb, username)
 	if id == "" {
 		return "", fmt.Errorf("user %v does not exists", username)
 	}
-	err = checkPassword(id, password)
+	err = checkPassword(rdb, id, password)
 	if err != nil {
 		return "", err
 	}
-	removeSession(id)
+	removeSession(rdb, id)
 	uuid := gouuid.NewV4().String()
 	err = rdb.HSet("user:"+id, "auth", uuid).Err()
 	if err != nil {
@@ -84,17 +116,12 @@ func LogIn(username, password string) (string, error) {
 	return uuid, nil
 }
 
-func LogOut(session string) error {
-	id, err := IsLoggedIn(session)
-	if err != nil {
-		return err
-	}
+func logOut(rdb *redis.Client, session string) error {
 	rdb.HDel("auths", session)
-	rdb.HDel("user:"+id, "auth")
 	return nil
 }
 
-func Follow(follower, followed string) error {
+func follow(rdb *redis.Client, follower, followed string) error {
 	var err error
 	currentTime := float64(time.Now().Unix())
 	_, err = rdb.ZAdd("followings:"+follower, redis.Z{
@@ -111,7 +138,7 @@ func Follow(follower, followed string) error {
 	return err
 }
 
-func Post(body, owner string) error {
+func post(rdb *redis.Client, body, owner string) error {
 	var err error
 	postID, err := rdb.Get(lastPostC).Result()
 	checkErr(err)
@@ -145,9 +172,9 @@ func Post(body, owner string) error {
 	return err
 }
 
-func showUserPosts(id string, start, count int64) (map[string][]string, error) {
+func showUserPosts(rdb *redis.Client, id string, start, count int64) (map[string][]string, error) {
 	//todo add models
-	if checkID(id) != nil {
+	if checkID(rdb, id) != nil {
 		return nil, nil
 	}
 	posts, err := rdb.LRange("posts:"+id, start, start+count).Result()
@@ -156,13 +183,13 @@ func showUserPosts(id string, start, count int64) (map[string][]string, error) {
 	}
 	postsMap := make(map[string][]string)
 	for _, post := range posts {
-		owner, body, _ := showPost(post)
+		owner, body, _ := showPost(rdb, post)
 		postsMap[post] = []string{owner, body}
 	}
 	return postsMap, nil
 }
 
-func showPost(postId string) (string, string, error) {
+func showPost(rdb *redis.Client, postId string) (string, string, error) {
 	post, err := rdb.HGetAll("post:" + postId).Result()
 	if err != nil {
 		return "", "", nil
@@ -170,18 +197,22 @@ func showPost(postId string) (string, string, error) {
 	return post["owner"], post["body"], nil
 }
 
-func removeSession(id string) error {
+func getSessionUserID(rdb *redis.Client, auth string) (string, error) {
+	return rdb.HGet("auths", auth).Result()
+}
+
+func removeSession(rdb *redis.Client, id string) error {
 	uuid, _ := rdb.HGet("user:"+id, "auth").Result()
 	_, _ = rdb.HDel("auths", uuid).Result()
 	return nil
 }
 
-func checkUsername(username string) string {
+func checkUsername(rdb *redis.Client, username string) string {
 	id, _ := rdb.HGet(usersMapC, username).Result()
 	return id
 }
 
-func checkID(id string) error {
+func checkID(rdb *redis.Client, id string) error {
 	exists, _ := rdb.HExists("user:"+id, "username").Result()
 	if !exists {
 		return fmt.Errorf("user %v not found", id)
@@ -189,17 +220,13 @@ func checkID(id string) error {
 	return nil
 }
 
-func checkPassword(id, password string) error {
+func checkPassword(rdb *redis.Client, id, password string) error {
 	pass, _ := rdb.HGet("user:"+id, passwordC).Result()
 	if pass != password {
 		log.Printf("id==%v pass %v != %v\n", id, pass, password)
 		return errors.New("invalid password")
 	}
 	return nil
-}
-
-func IsLoggedIn(auth string) (string, error) {
-	return rdb.HGet("auths", auth).Result()
 }
 
 func checkErr(err error) {
